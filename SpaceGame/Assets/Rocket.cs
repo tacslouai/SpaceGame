d6fc35464af522f65041e441e0917ee23262c45d
tacslouai <louaihammad2@gmail.com>
2020-02-25 11:06:32 -0500

    Added some sprites and scripts

---
+++ SpaceGame/Assets/Rocket.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    public Vector3[] rocketPos;
+    public int currPosIndex = 1;
+    public float t = 0;
+    public float moveSpeed = 7;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime * moveSpeed;
+        transform.position = Vector3.Lerp(transform.position, rocketPos[currPosIndex], t);
+    }
+}
